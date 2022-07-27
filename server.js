bebf627d468b3325549b84f5744d20c14b66eba4
richardleung1 <richardl57549@gmail.com>
2022-07-27 14:00:16 -0400

    Greeting express application created.

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8000
-o
+
+app.get('/greeting', (req, res) => {
+    res.send('Hello, stranger');
+});
+
+app.get('/greeting/:name', (req, res) => {
+    res.send('Wow! Hello there, ' + req.params.name + "! It's so great to see you!");
+});
 
 app.listen(port,() => {
     console.log('listening on port' , port)
