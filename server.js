74ebd66c87590aad6da59874047cb9c9f469111c
richardleung1 <richardl57549@gmail.com>
2022-07-27 14:00:55 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('/greeting/:name', (req, res) => {
     res.send('Wow! Hello there, ' + req.params.name + "! It's so great to see you!");
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tip = parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100)
+    res.send(`<p>${tip}</p>`);
+});
+
 app.listen(port,() => {
     console.log('listening on port' , port)
 })
